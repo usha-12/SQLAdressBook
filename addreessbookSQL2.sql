1e026e3805ed06ce2adf29aa06b2c5aecb952a66
usha-12 <usha12ahirwar@gmail.com>
2022-11-28 01:53:37 +0530

    Create Table

--- addreessbookSQL2.sql
+++ addreessbookSQL2.sql
@@ -1,3 +1,14 @@
 CREATE DATABASE AddressBookService1;
 USE AddressBookService1;
 select database();
+CREATE TABLE AddressBook(Id INT unsigned NOT NULL AUTO_INCREMENT,
+     First_Name VARCHAR(15) NOT NULL,
+     Last_Name VARCHAR(15) NOT NULL,
+     Address VARCHAR(150) NOT NULL,
+     City VARCHAR(15) NOT NULL,
+     State VARCHAR(15) NOT NULL,
+     Zip BIGINT NOT NULL,
+     Phone_Number BIGINT NOT NULL,
+	Email VARCHAR(150) NOT NULL,
+     PRIMARY KEY(Id)
+     );
